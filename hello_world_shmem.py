7e1f3c8fb078079980bd8f9c373f1b18296b2937
SamFicsor <m271878@usna.edu>
2026-04-21 08:47:37 -0400

    hello world example

---
+++ hello_world_shmem.py
@@ -0,0 +1,14 @@
+from shmem4py import shmem
+
+# no need to explicitly initialize shmem in shmem4py!
+
+# shmem version of comm sz and comm rank from MPI
+# comm size = n_pes()
+# comm rank = my_pe()
+my_rank = shmem.my_pe()
+commsz = shmem.n_pes()
+
+if my_rank == 0:
+    print('This is process 0! And I am amazing and I konw that')
+else:
+    print(f'hi process 0! I am {my_rank} of {commsz} processes')
